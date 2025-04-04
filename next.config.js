f3cf935c220f8d0d1ec3926c04c9b1d9545168fa
monciego <jerichobantiquete@gmail.com>
2025-04-05 00:12:34 +0800

    fix(build): update build scripts and next config

--- next.config.js
+++ next.config.js
@@ -6,13 +6,8 @@ const nextConfig = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
-  webpack(config) {
-    config.plugins.push(new VeliteWebpackPlugin()),
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-
+  webpack: (config) => {
+    config.plugins.push(new VeliteWebpackPlugin());
     return config;
   },
 };
