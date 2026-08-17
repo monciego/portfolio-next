8eb9bff4d4cbc9b352d520bc40c7c776fc18596f
monciego <jerichobantiquete@gmail.com>
2026-08-17 13:34:26 +0800

    chore: remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { useRestoreHomeScroll } from '@/lib/use-scroll-restoration';
 import { About } from '@/components/about';
 import Experience from '@/components/experience';
 import { Hero } from '@/components/hero';
 import { Mantra } from '@/components/mantra';
 import Projects from '@/components/projects';
-import { Terminal } from '@/components/terminal';
 import { Testimonials } from '@/components/testimonials';
+import { useRestoreHomeScroll } from '@/lib/use-scroll-restoration';
 
 export default function Home() {
   useRestoreHomeScroll();
